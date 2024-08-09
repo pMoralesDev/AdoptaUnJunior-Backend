7c9f813f1323c213d221d588676a0679c96b24e7
MoralesDev <151014369+pMoralesDev@users.noreply.github.com>
2024-08-09 11:56:35 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let name = 'MoralesDev'
+let age = 31
+let experience = '9 meses de practicas como desarrollador web'
+let edcuation = 'Tecnico Superior en Desarrollo de Aplicaciones Multiplataforma'
+
+function motivation () {
+  return `Hola mi nombre es ${name}. Actualmente trabajo como desarrollador web, principalmente maqueto wordpress y hago modulos personalizados. He realizado ${experience} y tengo el titulo de ${education}./n Llevo unos meses 
+  formandome en Node y Express para poder trabajar en Backend y estoy deseando poder colaborar en un proyecto real`
+}
+
+console.log(motivation())
